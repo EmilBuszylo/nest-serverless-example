520d19a1d7713168ff54896586d751ee231571f8
ebuszylo <emil.buszylo@gmail.com>
2021-03-29 16:19:54 +0200

    changees checker script test

---
+++ changeChecker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+REF=HEAD
+SINCE=origin/main
+DIR_TO_CHECK=src
+
+MERGE_BASE=$(git merge-base ${SINCE} ${REF})
+FILES_CHANGED=$(git diff --name-only ${MERGE_BASE}..${REF} -- ${DIR_TO_CHECK})
+printf "Files changed:\n${FILES_CHANGED}\n"
+
+if [[ -n $FILES_CHANGED ]]; then
+  echo "TRUE"
+else
+  echo "No changes"
+fi
